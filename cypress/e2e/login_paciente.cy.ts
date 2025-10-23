1af015194fe6aef7407da25a317efdf405aa4aed
Beatriz <btzolv26@gmail.com>
2025-10-23 20:59:37 -0300

    Login_paciente

--- cypress/e2e/login_paciente.cy.ts
+++ cypress/e2e/login_paciente.cy.ts
@@ -11,19 +11,19 @@ describe("Login paciente", () => {
     });
     it("Realizar login com senha curta", () => {
         cy.login(Cypress.env("senha_curta").email, Cypress.env("senha_curta").senha);
-        cy.url({ timeout: 20000 }). should("include", "/");
+        cy.url({ timeout: 20000 }).should("include", "/");
         cy.contains("Sua senha deve conter 8 ou mais caracteres").should("be.visible");
         cy.screenshot();
     });
     it("Realizar login com domínio inválido", () => {
         cy.login(Cypress.env("dominio_invalido").email, Cypress.env("dominio_invalido").senha)
-        cy.url({ timeout: 20000}). should("include", "/");
-        cy.contains("Insira um e-mail válido").should("be.visible");
+        cy.url({ timeout: 20000}).should("include", "/");
+        cy.contains("Credenciais inválidas. Por favor, tente novamente").should("be.visible");
         cy.screenshot();
     });
     it("Realizar login com campo email vazio", () => {
         cy.login(Cypress.env("email_vazio").email, Cypress.env("email_vazio").senha);
-        cy.url({ timeout: 20000 }). should("include", "/");
+        cy.url({ timeout: 20000 }).should("include", "/");
         cy.contains("Insira um e-mail válido").should("be.visible");
         cy.screenshot();
     });
@@ -32,7 +32,10 @@ describe("Login paciente", () => {
         cy.contains("Recuperar senha").should("be.visible");
         cy.screenshot();
     });
-    it("Realizar login com paciente", () => {
+    it("Realizar logout com paciente", () => {
+        cy.on('uncaught:exception', (err, runnable) =>{
+            return false;
+        });
         cy.login(Cypress.env("paciente").email, Cypress.env("paciente").senha);
         cy.url({ timeout: 20000 }).should("include", "/paciente");
         cy.contains('Iniciar triagem').should("be.visible");
